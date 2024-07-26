64847ec916ba57dd888709dc0b3e27bc9efa8e5e
4orty6yx <spincey1@gmail.com>
2024-07-27 00:12:58 +0800

    UPDATE

--- bmw/uptime.js
+++ bmw/uptime.js
@@ -47,7 +47,7 @@ function _0x4925() {
         'length',
         '\x20second',
         'QmFeS',
-        'y\x20BOT-MD',
+        'y\x204ORTY6YX-OFFICIAL-MD',
         'link...',
         '6108JYdMzG',
         '4|5|0|1|9|',
