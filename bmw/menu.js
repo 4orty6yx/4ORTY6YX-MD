1410784fedeb2f1c8406b42ef71e78fb08a2f63c
4orty6yx <spincey1@gmail.com>
2024-07-27 01:34:03 +0800

    menu

--- bmw/menu.js
+++ bmw/menu.js
@@ -39,7 +39,6 @@ const date = moment().format('DD/MM/YYYY');
 ┊ ┊ 🖱️𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀 : ${cm.length}
 ┊ ┊ 💻𝗕𝗼𝘁 𝗥𝗮𝗺 : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
 ┊ ┊ 🗓️𝗗𝗔𝗧𝗘 𝗧𝗢𝗗𝗔𝗬: : ${moment.tz('Africa/Nairobi').format('DD/MM/YYYY')}
-┊ ┊ 🚀𝗥𝗨𝗡𝗧𝗜𝗠𝗘 : ${runtime(process.uptime())}
 ┊ ┊ 🌍𝗢𝘂𝗿 𝘄𝗲𝗯𝘀𝗶𝘁𝗲: https://4orty6yxofficial.shop 
 ┊ ┊ 
 ┊ ╰─────────────────❂
