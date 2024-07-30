2383c990ad292eb44e8a21cc598fbca95cdd6c92
4orty6yx <spincey1@gmail.com>
2024-07-30 21:26:41 +0300

    update

--- bmw/ping.js
+++ bmw/ping.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "ping", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "ping", reaction: "✔", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*4ORTY6YX-MD IS ONLINE*  \n\n ' + "𝑻𝒉𝒆 𝒃𝒐𝒕 𝒊𝒔 𝒄𝒖𝒓𝒓𝒆𝒏𝒕𝒍𝒚 𝒘𝒐𝒓𝒌𝒊𝒏𝒈 𝒐𝒏 𝒂 𝒈𝒐𝒐𝒅 𝒔𝒑𝒆𝒆𝒅😉👍";
+    let z = '*4ORTY6YX-MD IS ONLINE*  \n\n ' + "Your bot is running at a good speed😉👍";
     let d = '                                                                           BOT STATUS: HEALTHY✨';
     let varmess = z + d;
-    var mp4 = 'https://telegra.ph/file/ce58cf8c538b1496fda33.mp4';
+    var mp4 = 'https://telegra.ph/file/a21d5a0d54fce96b2ced2.jpg';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
     //console.log("montest")
 });
